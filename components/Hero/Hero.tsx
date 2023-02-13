01ef8429665aab01351a0ab426f36c26d9c13673
Heather Preiss <heather.l.preiss@gmail.com>
2023-02-13 16:59:34 -0600

    fixed hero svg class name switch, ensured styles were acurately working

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -19,41 +19,77 @@ const Hero = ({
   const isBasic = variant === "basic";
   const isPrimary = variant === "primary";
   const isSecondary = variant === "secondary";
-  const isDogGroups = (leftGraphic === "goldenGroup" || leftGraphic === "terrierGroup") || (rightGraphic === "goldenGroup" || rightGraphic === "terrierGroup");
+  const isDogGroups = leftGraphic === "goldenGroup" || leftGraphic === "terrierGroup" || rightGraphic === "goldenGroup" || rightGraphic === "terrierGroup";
   const isDonate = variant === "donate";
 
   if (isDonate) {
     return <DonateHero title={title ? title : null} />;
   }
+
+  let svgClassLeft;
+  let sideGraphicLeft;
+  if (isBasic) {
+    svgClassLeft = "-left-8 xs:-left-20 h-full max-w-full xs:max-w-[60%] md:-left-28 lg:-left-8 2xl:left-0 basicSvg";
+  } else if (isDogGroups) {
+    svgClassLeft = "-left-4 h-full max-w-[50%] self-start sm:-left-12 md:-left-16 2xl:left-0 dogGrpSvg";
+  } else {
+    svgClassLeft = "-left-20 h-full max-w-[50%] self-start xs:-left-12 md:-left-16 2xl:left-0 allSvg";
+  }
+  if (isBasic) {
+    sideGraphicLeft = "relative bottom-0 h-full max-h-full basicSvg";
+  } else if (leftGraphic === "volunteerWalker") {
+    sideGraphicLeft = "h-full max-h-[190px] md:max-h-[300px] volSvg";
+  } else if (isDogGroups) {
+    sideGraphicLeft = "absolute bottom-0 h-[250px] max-h-[35vw] xs:max-h-[30vw] md:max-h-[75%] lg:h-[300px] dogGrpSvg";
+  } else {
+    sideGraphicLeft = "absolute bottom-0 h-[160px] max-h-[47vw] sm:max-h-[42%] sm:h-[225px] md:max-h-[75%] lg:h-[300px] allSvg";
+  }
+
+  let svgClassRight;
+  let sideGraphicRight;
+  if (isBasic) {
+    svgClassRight = "-right-20 h-full hidden xs:flex max-w-[60%] md:-right-28 md:justify-end lg:-right-8 2xl:right-0 basicSvg";
+  } else if (isDogGroups) {
+    svgClassRight = "-right-14 h-full max-w-[50%] justify-end self-end sm:-right-20 md:-right-16 2xl:right-0 dogGrpSvg";
+  } else {
+    svgClassRight = "-right-20 h-full max-w-[50%] justify-end self-end xs:-right-12 md:-right-16 2xl:right-0 allSvg";
+  }
+  if (isBasic) {
+    sideGraphicRight = "relative bottom-0 h-full max-h-full -scale-x-100 basicSvg";
+  } else if (rightGraphic === "volunteerWalker") {
+    sideGraphicRight = "h-full max-h-[190px] md:max-h-[300px] volSvg";
+  } else if (isDogGroups) {
+    sideGraphicRight = "absolute bottom-0 h-[250px] max-h-[35vw] xs:max-h-[30vw] -scale-x-100 md:max-h-[75%] lg:h-[300px] dogGrpSvg";
+  } else {
+    sideGraphicRight = "absolute bottom-0 h-[160px] max-h-[47vw] sm:max-h-[42%] -scale-x-100 sm:h-[225px] md:max-h-[75%] lg:h-[300px] allSvg";
+  }
+
+  let rightGraphicValue;
+  if (rightGraphic === "volunteerWalker") {
+    rightGraphicValue = "volunteerWalkerRight";
+  } else {
+    rightGraphicValue = rightGraphic;
+  }
+
+  // else if (isBasic) {
+  //   rightGraphicValue = "";
+  // } 
+  
   return (
     <div
       className={`${className ? className : ``} border-b-[29px] border-primary`}
     >
       <div
-        className={`${isPrimary ? `min-h-[450px]` : `h-fit`} ${
+        className={`${isPrimary ? `min-h-[400px] md:min-h-[450px]` : `h-fit`} ${
           isBasic ? "pb-0" : "pb-28 md:pb-40 lg:pb-0"
         } relative z-0 mx-auto flex max-w-screen-2xl flex-col items-center text-center md:flex-row`}
       >
         {leftGraphic ? (
           <div
-            className={`${
-              isBasic
-                ? "-left-8 xs:-left-20 h-full max-w-full xs:max-w-[60%] md:-left-28 lg:-left-8 2xl:left-0"
-                : (isDogGroups
-                ? "-left-12 h-full max-w-[50%] self-start xs:-left-12 sm:-left-20 md:-left-16 2xl:left-0"
-                : "-left-20 h-full max-w-[50%] self-start xs:-left-12 md:-left-16 2xl:left-0")
-            } absolute bottom-0 flex w-full justify-start`}
+            className={`${svgClassLeft} absolute bottom-0 flex w-full justify-start`}
           >
             <SideGraphic
-              className={`${
-                isBasic
-                  ? "relative bottom-0 h-full max-h-full"
-                  : (leftGraphic === "volunteerWalker"
-                  ? "h-full max-h-[190px] md:max-h-[300px]"
-                  : (isDogGroups
-                  ? "absolute bottom-0 h-[250px] max-h-[30%] sm:max-h-[40%] md:max-h-[75%] lg:h-[300px]"
-                  : "absolute bottom-0 h-[160px] max-h-[35%] sm:h-[225px] md:max-h-[75%] lg:h-[300px]"))
-              }`}
+              className={`${sideGraphicLeft}`}
               graphic={isBasic ? "" : leftGraphic}
             />
           </div>
@@ -91,31 +127,11 @@ const Hero = ({
         </div>
         {rightGraphic ? (
           <div
-            className={`${
-              isBasic
-                ? "-right-20 h-full hidden xs:flex max-w-[60%] md:-right-28 md:justify-end lg:-right-8 2xl:right-0"
-                : (isDogGroups
-                ? "-right-[5.5rem] h-full max-w-[50%] justify-end self-end xs:-right-12 sm:-right-[7.5rem] md:-right-16 2xl:right-0"
-                : "-right-20 h-full max-w-[50%] justify-end self-end xs:-right-12 md:-right-16 2xl:right-0")
-            } absolute bottom-0 flex w-full`}
+            className={`${svgClassRight} absolute bottom-0 flex w-full`}
           >
             <SideGraphic
-              className={`${
-                isBasic
-                  ? "relative bottom-0 h-full max-h-full -scale-x-100"
-                  : (rightGraphic === "volunteerWalker"
-                  ? "h-full max-h-[190px] md:max-h-[300px]"
-                  : (isDogGroups
-                  ? "absolute bottom-0 h-[250px] max-h-[30%] -scale-x-100 sm:max-h-[40%] md:max-h-[75%] lg:h-[300px]"
-                  : "absolute bottom-0 h-[160px] max-h-[35%] -scale-x-100 sm:h-[225px] md:max-h-[75%] lg:h-[300px]"))
-              }`}
-              graphic={
-                isBasic
-                  ? ""
-                  : (rightGraphic === "volunteerWalker"
-                  ? "volunteerWalkerRight"
-                  : rightGraphic)
-              }
+              className={`${sideGraphicRight}`}
+              graphic={`${isBasic ? "" : rightGraphicValue}`}
             />
           </div>
         ) : null}
